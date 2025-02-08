4ec3712c96ed2bbf5b8100e840ae5dba0dc2aa93
Yiannis <stav0049@gmail.com>
2025-02-07 21:43:02 -0600

    start files

---
+++ LightningTracker.API/Models/TodoContext.cs
@@ -0,0 +1,12 @@
+namespace LightningTracker.API.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; } = null!;
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Project_Management_Issue_Tracking
+
+# Enhanced TodoItem WebAPI
+
+https://learn.microsoft.com/en-us/aspnet/core/tutorials/first-web-api?view=aspnetcore-9.0&tabs=visual-studio-code
